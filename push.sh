aa43a9f8c2781fecf9d5d66805e749615ff20f3e
Cryptide <valveunion76@gmail.com>
2018-12-21 22:24:58 +0300

    21.12.2018 22:24:58

--- push.sh
+++ push.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-timestamp1() {
-    date +"%d.%m.%Y %H:%M:%S"
-}
-
-timestamp=$(date +%s)
-DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
-
-# git reset HEAD~
 git add -A
 git commit -m "$( date +"%d.%m.%Y %H:%M:%S" )"
 git push
